94282f07c52910234f2b8860a48724210a5b043d
Alexander Kvamme <alexanderkvamme@gmail.com>
2019-06-14 16:07:47 +0200

    Initial commit

---
+++ horizontalcards/horizontalcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  horizontalcards
+//
+//  Created by Amia Macone on 14/06/2019.
+//  Copyright © 2019 Amia Macone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
