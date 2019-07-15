dea38e1b437a2fa6271c20ecd6e1491b67355737
Alexander Kvamme <alexanderkvamme@gmail.com>
2019-07-15 11:22:38 +0200

    add properly fonted labels to cards

--- horizontalcards/horizontalcards/CardCell.swift
+++ horizontalcards/horizontalcards/CardCell.swift
@@ -17,23 +17,56 @@ final class CardCell: UICollectionViewCell {
     static let identifier = "Card cell"
     static let estimatedItemSize = CGSize(width: UIScreen.main.bounds.width-CardController.horizontalInsets*2, height: 150)
     
-    private let label = UILabel()
+    private let headerLabel = UILabel()
+    private let valueLabel = UILabel()
+    private let mvaLabel = UILabel()
+    private let accountLabel = UILabel()
     
     // MARK: - Initializers
     
     override init(frame: CGRect) {
         super.init(frame: .zero)
+
+        headerLabel.text = "test"
+        headerLabel.font = UIFont.kolibrix.bold
+        headerLabel.textColor = UIColor.solarstein.seashell
         
-        label.text = "test"
-        label.font = UIFont.kolibrix.header
-        label.textColor = UIColor.solarstein.seashell
+        valueLabel.font = UIFont.kolibrix.subheader
+        valueLabel.textColor =  UIColor.solarstein.seashell
+        valueLabel.text = "399 kr"
+        
+        mvaLabel.font = UIFont(name: "RobotoMono-Bold", size: 16)
+        mvaLabel.textColor = UIColor.solarstein.salmon
+        mvaLabel.text = "Legg til MVA"
+        
+        accountLabel.font = UIFont(name: "RobotoMono-Bold", size: 16)
+        accountLabel.textColor = UIColor.solarstein.salmon
+        accountLabel.text = "Legg til konto"
         
         backgroundColor = UIColor.solarstein.sapphire
         
-        contentView.addSubview(label)
-        label.snp.makeConstraints { (make) in
+        contentView.addSubview(headerLabel)
+        headerLabel.snp.makeConstraints { (make) in
             make.top.left.equalToSuperview().offset(16)
             make.right.equalToSuperview()
+        }
+        
+        contentView.addSubview(mvaLabel)
+        mvaLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(headerLabel.snp.bottom).offset(17)
+            make.left.equalToSuperview().offset(16)
+        }
+        
+        contentView.addSubview(accountLabel)
+        accountLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(mvaLabel.snp.bottom)
+            make.left.equalToSuperview().offset(16)
+        }
+        
+        contentView.addSubview(valueLabel)
+        valueLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(accountLabel.snp.bottom).offset(16)
+            make.left.equalToSuperview().offset(16)
         }
         
         contentView.snp.makeConstraints { (make) in
@@ -53,7 +86,7 @@ final class CardCell: UICollectionViewCell {
     // MARK: Methods
     
     func update(with data: String) {
-        label.text = data
+        headerLabel.text = data
     }
     
     func setFaded(_ b: Bool) {
